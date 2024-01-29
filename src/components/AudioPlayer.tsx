d37b7b5d2017750eb98993d2ef89e365517d83aa
jerry <114961129+jjxrry@users.noreply.github.com>
2024-01-29 15:54:14 -0800

    feat: add shuffle functionality

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,32 +1,55 @@
 import React, { useEffect, useState } from "react";
 
 const AudioPlayer: React.FC = () => {
-    const songUrl = 'src/assets/songs/Rich_Girl_Hall_and_Oates.mp3';
+    // const songUrl = 'src/assets/songs/Rich_Girl_Hall_and_Oates.mp3';
+    const songList = [
+        {url: 'src/assets/songs/Rich_Girl_Hall_and_Oates.mp3', title: 'Rich Girl', artist: 'Hall and Oates'},
+        {url: 'src/assets/songs/Crying_in_a_Loop_Mateus_Asato.mp3', title: 'Crying in a Loop', artist: 'Mateus Asato'},
+        {url: 'src/assets/songs/Higgs_Frank_Ocean.mp3', title: 'Higgs', artist: 'Frank Ocean'},
+        {url: 'src/assets/songs/Blonde_Compilation_Frank_Ocean.mp3', title: 'Blonde Compilation', artist: 'Frank Ocean'}
+    ]
 
-    const [currentSong, setCurrentSong] = useState(new Audio(songUrl));
+    
+    const [currentSong, setCurrentSong] = useState(new Audio(songList[0].url));
+    const [currentInfo, setCurrentInfo] = useState(songList[0]);
     const [isPlaying, setIsPlaying] = useState(false);
+    
+    const getRandomIndex = () => Math.floor(Math.random() * songList.length)
 
     useEffect(() => {
         isPlaying ? currentSong.play() : currentSong.pause();
 
         return () => {
             currentSong.pause();
-            currentSong.currentTime = 0;
+            // currentSong.currentTime = 0;
         }
     }, [isPlaying, currentSong])
 
-    const togglePlayPause = () => {
-        if (currentSong.src != songUrl){
-            setCurrentSong(new Audio(songUrl));
+    const loadRandom = () => {
+        const random = getRandomIndex()
+        const songUrl = songList[random];
+        setCurrentInfo(songUrl);
+
+        if (currentSong.src != songUrl.url){
+            setCurrentSong(new Audio(songUrl.url));
         }
+        setIsPlaying(true);
+    }
+
+    const togglePlayPause = () => {
         setIsPlaying(!isPlaying);
     }
     
     return (
         <div>
             <button onClick={togglePlayPause}>
-                {isPlaying ? "pause" : "play"}
+                {isPlaying ? "Pause" : "Play"}
             </button>
+            <button onClick={loadRandom}>
+                Shuffle
+            </button>
+            <p>Title: {currentInfo.title}</p>
+            <p>Artist: {currentInfo.artist}</p>
         </div>
     )
 }
